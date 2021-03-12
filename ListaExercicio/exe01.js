bf36e8b5c84d3227f4c4cb1e2c92b30570c9c804
Diogo Deconto <dideconto@gmail.com>
2021-03-12 11:41:03 -0300

    Lista de Exercício

---
+++ ListaExercicio/exe01.js
@@ -0,0 +1,10 @@
+
+let altura = Math.floor(Math.random() * 100);
+let largura =  Math.floor(Math.random() * 100);
+let area = altura * largura;
+
+console.log(altura, largura, area);
+console.log("Área: " + area + " m2");
+
+//Interpolação - Template Literals
+console.log(`Área: ${area} m2`);
---
+++ ListaExercicio/exemplo.js
@@ -0,0 +1,41 @@
+let variavel = 123;
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = 123.505;
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = "123";
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = true;
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = { nome: "Diogo Deconto" };
+console.log(variavel);
+console.log(typeof variavel);
+
+//----------------------------------------
+
+myName = 'Chris';
+
+function logName() {
+  console.log(myName);
+}
+
+logName();
+
+var myName;
+
+//----------------------------------------
+
+console.log("123" === 123);
+
+if("123" === 123){
+    console.log("Igual");
+}else{
+    console.log("Diferente");
+}
