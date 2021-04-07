ba43297aeb61df1ebf520b5f7c68b4276580dd95
Diogo Deconto <dideconto@gmail.com>
2021-04-07 12:09:45 -0300

    Requisições com GET e POST, parâmestros da URL e do corpo da requisição

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,15 +1,47 @@
-import express from "express";
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
 const app = express();
 
 console.clear();
 
+app.use(express.json());
 //Métodos HTTP ->
 //GET  -> Solicita um determinado recurso do servidor
 //POST -> Envia um recurso para o servidor
 
 app.get("/", (request: Request, response: Response) => {
-  response.send("Hello World com ts-node-dev!");
+  response.send("<h1>Hello World com ts-node-dev!</h1>");
+});
+
+app.get("/ciclo/listar", (request: Request, response: Response) => {
+  const objeto = {
+    atribTexto: "Uma string qualquer",
+    atribNumero: 123.54,
+    atribLogico: false,
+    atribObjeto: {
+      atribVetor: ["Opção1", "Opção 2"],
+    },
+  };
+  response.json(objeto);
+});
+
+app.get("/ciclo/listar/:param1/:param2", (request: Request, response: Response) => {
+  const { param1, param2 } = request.params;
+  const objeto = {
+    p1: param1,
+    p2: param2,
+    atribTexto: "Uma string qualquer",
+    atribNumero: 123.54,
+    atribLogico: false,
+    atribObjeto: {
+      atribVetor: ["Opção1", "Opção 2"],
+    },
+  };
+  response.json(objeto);
+});
+
+app.post("/usuario/cadastrar", (request: Request, response: Response) => {
+  console.log(request.body);
+  response.json("Criamos uma funcionalidade para receber dados!");
 });
 
 app.listen(3000, () => {
